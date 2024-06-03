70954e2eb43b3beb96df15bd0df8527cc310a643
newpc <leilyesdu06300@gmail.com>
2024-06-03 15:03:26 +0200

    2

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isouaidi <isouaidi@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 15:17:24 by isouaidi          #+#    #+#             */
+/*   Updated: 2024/06/01 15:26:44 by isouaidi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_H
+# define WEAPON_H
+
+#include <iostream>
+
+class Weapon
+{
+	private:
+		std :: string  name;
+	public:
+	
+	
+		Weapon();
+		~Weapon();
+};
+
+#endif
